f4094d5e2e63ee4b38badc5af55b7766c0b5988e
Ana de la Jara <ana.delajara@the-cocktail.com>
2022-05-23 18:13:05 +0200

    conect with api

--- src/dogs/dogs.controller.ts
+++ src/dogs/dogs.controller.ts
@@ -1,12 +1,12 @@
-import { Controller } from '@nestjs/common';
-import { DogsService } from './dogs.service';
+import { Controller, Get } from '@nestjs/common';
+import { callToApi, DogsService } from './dogs.service';
 
 @Controller('dogs')
 export class DogsController {
   constructor(private servicioDePerretes: DogsService) {}
 
   @Get()
-  getallBreeds() {
-    return this.getallBreeds.call();
+  callToApi() {
+    return callToApi();
   }
 }
--- src/dogs/dogs.service.ts
+++ src/dogs/dogs.service.ts
@@ -1,7 +1,16 @@
-import { Injectable } from '@nestjs/common';
-import { Axios } from 'axios';
+import { HttpException, Injectable } from '@nestjs/common';
+import { catchError, map } from 'rxjs/operators';
+import { AxiosResponse } from 'axios';
+import { IGetAllDogs } from './dogs.interfaces';
+
+const axios = require('axios').default;
 
 @Injectable()
-export class DogsService {
-  private dogs;
+export class DogsService {}
+
+export async function callToApi() {
+  const apiAwait = await axios.get('https://dog.ceo/api/breeds/list/all');
+  console.log(apiAwait.data);
+  const response: IGetAllDogs = apiAwait.data;
+  return response.message.malamute;
 }
