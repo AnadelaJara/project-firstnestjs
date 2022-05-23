01871d16f05b4d009445bdebeee14ed85cbd57b6
Ana de la Jara <ana.delajara@the-cocktail.com>
2022-05-23 13:15:49 +0200

    first commit feature/dogs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { CatsModule } from './cats/cats.module';
+import { DogsController } from './dogs/dogs.controller';
 
 @Module({
   imports: [CatsModule],
+  controllers: [DogsController],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
