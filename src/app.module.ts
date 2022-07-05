445ba7782fd943615eaeb4114fb61f87af536a39
Ana de la Jara <ana.delajara@the-cocktail.com>
2022-07-05 13:34:37 +0200

    add mysql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { CatsModule } from './cats/cats.module';
 import { DogsModule } from './dogs/dogs.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CatsModule, DogsModule],
+  imports: [
+    CatsModule,
+    DogsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'testa',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
